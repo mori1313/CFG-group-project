0312bf934c486c295e242d6b1ec83b450bb81e38
nicolap16 <66072944+nicolap16@users.noreply.github.com>
2020-07-07 17:09:56 +0100

    Add files via upload

---
+++ CFG-group-project-moriayo/Group Project/JavaScript/app.js
@@ -0,0 +1,10 @@
+
+function showMenu() {
+    var x = document.getElementById("phoneNav");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
